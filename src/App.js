b86928c19d5e36f36872f78b2b12df80a78bb651
SoheilTajik <soheil.tajik@gmail.com>
2024-02-05 15:47:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div className="intro">
+        <h1>
+          Learn how to make <strong>the best BBQ ribs</strong> in town
+        </h1>
+        <p>Join us for this live webinar</p>
+        <p className="top-text">Mouthwateringly delicious</p>
+      </div>
+      <div className="main-content">
+        <h2>Become a BBQ master!</h2>
+        <p>Register Today</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BBQ isn't just standing in front of your grill with it on full blast
+          and hoping for the best. It's an art! One way to speed up the process
+          is to learn from the best. You can do just that by signing up for this
+          free webinar!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <form className="email-collector" action="#" method="post">
+          <input type="text" placeholder="first name" required />
+
+          <input type="email" name="" id="" placeholder="email" required />
+
+          <button className="btn btn-primary" type="submit">
+            Register
+          </button>
+        </form>
+        <p className="fine-print">
+          We'll never share your information without your permission
+        </p>
+      </div>
     </div>
   );
 }
